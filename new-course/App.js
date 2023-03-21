815877605bd38221a03a40927ade763d200b1ddc
david <davidmatovu88@gmail.com>
2023-03-21 10:32:51 +0300

    styling react native apps

--- new-course/App.js
+++ new-course/App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.dummyText}>Week For React Native</Text>
+      </View>
+      <Text
+        style={{ margin: 16, borderWidth: 2, borderColor: "red", padding: 16 }}
+      >
+        Moving forward
+      </Text>
+      <Button title="Click Here" />
     </View>
   );
 }
@@ -13,8 +20,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff", 
+    alignItems: "center",
+    justifyContent: "center",
   },
+  dummyText: {
+    margin: 16,
+    borderWidth: 2,
+    borderColor:"red",
+    padding: 16
+  }
 });
