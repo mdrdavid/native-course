712430eb3161983d7db3682838331ef8823a0858
david <davidmatovu88@gmail.com>
2023-03-21 19:31:32 +0300

    add layouts

--- new-course/App.js
+++ new-course/App.js
@@ -1,33 +1,35 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.appContainer}>
       <View>
-        <Text style={styles.dummyText}>Week For React Native</Text>
+        {/* <Text style={styles.dummyText}>Week For React Native</Text> */}
+        <TextInput placeholder="your course goal!" />
+        <Button title="Add Goal" />
       </View>
-      <Text
+      <View>
+        <Text>List of goals...</Text>
+      </View>
+      {/* <Text
         style={{ margin: 16, borderWidth: 2, borderColor: "red", padding: 16 }}
       >
         Moving forward
       </Text>
-      <Button title="Click Here" />
+      <Button title="Click Here" /> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff", 
-    alignItems: "center",
-    justifyContent: "center",
+  appContainer: {
+    padding: 50,
   },
   dummyText: {
     margin: 16,
     borderWidth: 2,
-    borderColor:"red",
-    padding: 16
-  }
+    borderColor: "red",
+    padding: 16,
+  },
 });
