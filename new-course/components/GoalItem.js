41004863658f7f5dfae6c3d46dde308f0d4178f7
david <davidmatovu88@gmail.com>
2023-03-25 11:34:10 +0300

    add andiroid ripple

--- new-course/components/GoalItem.js
+++ new-course/components/GoalItem.js
@@ -2,11 +2,15 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem({ text, onDeleteItem }) {
   return (
-    <Pressable onPress={onDeleteItem.bind(this.id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        onPress={onDeleteItem.bind(this.id)}
+        android_ripple={{ color: "#210064" }}
+        style={({ pressed }) => pressed && styles.pressedItem} // for ios ripple
+      >
         <Text style={styles.goalText}>{text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
@@ -15,11 +19,14 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
   },
   goalText: {
     color: "white",
+    padding: 8,
+  },
+  pressedItem: {
+    opacity: 0.5,
   },
 });
